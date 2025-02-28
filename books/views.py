6d428ce0d63dca7b27ce00d1179c3888708366a1
Mohsen Hajizadeh <m.hajizadeh2023@gmail.com>
2025-02-28 12:31:25 +0330

    add create view and update view

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse
 
 from .models import Book
 
@@ -10,3 +11,16 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'description', 'author', 'price',]
+    template_name = 'books/book_create.html'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'description', 'author', 'price',]
+    template_name = 'books/book_update.html'
+
+    def get_success_url(self):
+        return reverse('book_detail', kwargs={'pk': self.object.pk})
